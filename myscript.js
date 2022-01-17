9d62873de13e06c37a12c3b4c8529d1b5f513d0b
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-01-17 15:52:54 +0100

    creo new vue

---
+++ myscript.js
@@ -0,0 +1,27 @@
+let app = new Vue({
+    el: '#myapp',
+    data : {
+        todo:[
+            {
+                text:'Discutere con Milan',
+                done: 'false'
+            },
+            {
+                text:'Portare a spasso il cane',
+                done: 'false'
+            },
+            {
+                text:'Aperitivo al Maboo',
+                done: 'false'
+            },
+            {
+                text:'Andare al circo di scacchi',
+                done: 'false'
+            },
+            {
+                text:'Esercitarsi con la chitarra',
+                done:'false'
+            }
+        ]
+    }
+})
