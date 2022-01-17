29e0d75b3eb4241ddf4bcab04e6e50f1bdbbdc75
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-01-17 19:09:27 +0100

    aggiungo funzione deleteObj

--- myscript.js
+++ myscript.js
@@ -5,43 +5,36 @@ let app = new Vue({
         todo:[
             {
                 text:'Discutere con Milan',
-                done: 'false'
-            },
-            {
-                text:'Portare a spasso il cane',
-                done: 'false'
-            },
-            {
-                text:'Aperitivo al Maboo',
-                done: 'false'
-            },
-            {
-                text:'Andare al circo di scacchi',
-                done: 'false'
-            },
-            {
-                text:'Esercitarsi con la chitarra',
-                done:'false'
-            },
-
-            
+                done: false
+            }
         ]
-
     },
     methods:{
-        //funzione che aggiunge obj all'array 'todo' => elemento nella "to do list"
+
+        //funzione che aggiunge obj all'array 'todo' => elemento nella "to do list".
         addObj: function(){
             
             let obj= {
                 text :this.valoreTesto ,
-                done:'false'
+                done: false
             }
             this.todo.push(obj);
         },
-        //funzione che rimuove obj dalla "to do list"
+
+        //funzione che rimuove obj dalla "to do list".
         removeObj: function(indice){
             this.todo.splice(indice,1);
-            return indice
+        },
+        
+        //funzione che assegna classe .delete al click sullo span.
+        deleteObj : function(indice){
+            if(this.todo[indice].done == false){
+                this.todo[indice].done = true
+                
+            }else{
+                this.todo[indice].done = false
+            }
+            
         }
     }
 })
